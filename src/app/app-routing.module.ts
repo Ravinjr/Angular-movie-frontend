06b90fb57b6a751fd13b2debe9fb2024701a1cb0
Raindu <101939207+Raindu@users.noreply.github.com>
2024-05-27 17:33:05 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import path from 'node:path';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},//when there is no path 
+  {path:'login',component:LoginComponent},
+  {path:'home',component:HomeComponent},
+  {path:'movie',component:MovieComponent},
+  {path:'**',component:LoginComponent}//when no path matches
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
